7d99c1d19691998157cfec0c1649c64f73765bb7
magdielmarques <contato.magdiel@gmail.com>
2022-05-29 11:43:07 -0400

    send email method

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,37 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtraIMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: '',
+      port: ,
+      auth: {
+        user: '',
+        pass:''
+      }
+    })
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    })
+  }
+}
+
+/* 
+Adicionar suas proprias credenciais do mailtrap
+*/
